fed6b8a515cbcb976d7276fadf195dab2d7e856d
afreireosorio <alexandreosorio@yahoo.com>
2022-02-09 17:45:48 -0300

    Adaptacao para serializar o modelo HAR.

--- examples/android/tflite_convertor/harmodel_ori.py
+++ examples/android/tflite_convertor/harmodel_ori.py
@@ -15,46 +15,21 @@ from tfltransfer.tflite_transfer_converter import TFLiteTransferConverter
 warnings.filterwarnings('ignore')
 from data import Data
 
-"""Define the base model.
 
-To be compatible with TFLite Model Personalization, we need to define a
-base model and a head model. 
-
-Here we are using an identity layer for base model, which just passes the 
-input as it is to the head model.
-"""
-
-class BaseModel(tf.keras.Sequential):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.data = Data()
-        self.init_model()
-        self.compile(loss="categorical_crossentropy", optimizer="sgd")
-        self.save("identity_model", save_format="tf")
-
-    def init_model(self):
-        self.add(tf.keras.Input(shape=self.data.x_train.shape[1]))
-        self.add(tf.keras.layers.Lambda(lambda x: x))
-
-
-"""Define the head model.
-
-This is the model architecture that we will train using Flower. 
-"""
 class HarModel(tf.keras.Sequential):
     def __init__(self) -> None:
         super().__init__()
 
         self.data = Data()
         self.init_model()
+        # self.harclient = HarClient()
 
     def init_model(self):
         self.add(
             Dense(units=64, kernel_initializer='normal', activation='sigmoid', input_dim=self.data.x_train.shape[1]))
         self.add(Dropout(0.2))
         self.add(Dense(units=6, kernel_initializer='normal', activation='softmax'))
-        self.compile(optimizer='adam', loss='categorical_crossentropy')
+        self.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     def build_model(self, hp):
         model = keras.Sequential()
@@ -85,13 +60,42 @@ class HarModel(tf.keras.Sequential):
         self.model=tuner.get_best_models(num_models=1)[0]
 
 
+# (to generate a SavedModel) tf.saved_model.save(model, "saved_model_keras_dir")
+model = HarModel()
 
-baseModel = BaseModel()
-headModel = HarModel()
+# Convert the model.
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model.
+with open('harmodel.tflite', 'wb') as f:
+    f.write(tflite_model)
+
 
 """Convert the model for TFLite.
 
-Using 6 classes in HAR, learning rate = 1e-3 and batch size = 64
+Using 10 classes in CIFAR10, learning rate = 1e-3 and batch size = 32   
+
+This will generate a directory called tflite_model with five tflite models.
+Copy them in your Android code under the assets/model directory.
+"""
+
+
+head = tf.keras.Sequential(
+    [
+        tf.keras.Input(model.data.x_train.shape[1]),
+        tf.keras.layers.Dense(units=64, kernel_initializer='normal', activation='sigmoid'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(units=6, kernel_initializer='normal', activation='softmax'),
+    ]
+)
+
+#head.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+head.compile(optimizer='adam', loss='categorical_crossentropy')
+
+"""Convert the model for TFLite.
+
+Using 6 classes in HAR, learning rate = 1e-3 and batch size = 32
 
 This will generate a directory called tflite_model with five tflite models.
 Copy them in your Android code under the assets/model directory.
@@ -99,7 +103,7 @@ Copy them in your Android code under the assets/model directory.
 
 base_path = bases.saved_model_base.SavedModelBase("identity_model")
 converter = TFLiteTransferConverter(
-    6, base_path, heads.KerasModelHead(headModel), optimizers.SGD(1e-3), train_batch_size=64
+    6, base_path, heads.KerasModelHead(head), optimizers.SGD(1e-3), train_batch_size=64
 )
 
 converter.convert_and_save("tflite_model")
